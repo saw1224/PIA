c10246171ba868e609da0489df0b426c51f27708
Saul Edrei <edreirdz2003@gmail.com>
2024-05-12 20:57:57 -0600

    se agrgaron los de mas indexs

---
+++ PIA/script.js
@@ -0,0 +1,5 @@
+// Agregar evento de clic al botón de agendar cita
+document.getElementById("agendar-cita-btn").addEventListener("click", function() {
+    // Redirigir al usuario a la página deseada
+    window.location.href = "citas.html"; // Cambia "otro_index.html" por la URL de tu otro índice
+});
