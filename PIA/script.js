9286d6d7154414016e2d78fc2ee06c785aeb3531
Saul Edrei <edreirdz2003@gmail.com>
2024-05-13 02:17:42 -0600

    ultimo comit dia 13 cambiar imagenes del crrusel

--- PIA/script.js
+++ PIA/script.js
@@ -3,3 +3,7 @@ document.getElementById("agendar-cita-btn").addEventListener("click", function()
     // Redirigir al usuario a la página deseada
     window.location.href = "citas.html"; // Cambia "otro_index.html" por la URL de tu otro índice
 });
+
+var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleSlidesOnly'), {
+    interval: 2000 // Intervalo de cambio de diapositivas en milisegundos
+});
